d8559ede6dfa56b6b9a84621f3224d2a22b4a321
herrscherin <inijohan29@gmail.com>
2025-02-16 23:39:09 +0700

    V1 - R30 - HP14s
    Version 1
    Revision 30
    on HP14s laptop

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    home-manager = {
+    	url = "github:nix-community/home-manager";
+    	inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {	self, 
+  		nixpkgs,
+		home-manager, 
+		zen-browser, ... } @ inputs:
+  let
+  	system = "x86_64-linux";
+	pkgs = import nixpkgs {
+		inherit system;
+		config = {
+			allowUnfree = true;
+		};
+	       };
+  in 
+  {
+    nixosConfigurations = {
+    nixos = nixpkgs.lib.nixosSystem { 
+     specialArgs = { inherit inputs system; };
+     modules = [
+      ./configuration.nix
+      home-manager.nixosModules.home-manager {
+      home-manager.useGlobalPkgs = true;
+      }
+     ];
+    };
+  };
+  };
+}
---
+++ pkgs.nix
@@ -0,0 +1,32 @@
+{ pkgs, config, inputs, ... } :
+
+{
+
+home.packages = with pkgs; [
+	git
+	gimp
+	libreoffice-fresh
+	aseprite
+	inputs.zen-browser.packages."${system}".default
+	ranger
+	lazygit
+	p7zip
+	nchat
+	edk2
+	edk2-uefi-shell
+	mpv
+	nicotine-plus
+	legcord
+	easyeffects
+	bunnyfetch
+	gcc14
+	peaclock
+	musikcube
+	htop
+	guake
+	abaddon
+	discordo
+	git-credential-manager
+];	
+
+}
